cef8f8db2e76c98de4bdbc82cbc5d5735d1d3a67
wdwang <wdwang09+git@outlook.com>
2022-06-13 16:55:07 +0800

    quicksort

--- sort/quick_sort.h
+++ sort/quick_sort.h
@@ -2,40 +2,23 @@
 
 #pragma once
 
+#include <functional>
 #include <vector>
 
 namespace {
 
-template <typename T>
-void q_sort(std::vector<T> &v, std::size_t begin, std::size_t end);
-
-template <typename T>
-std::size_t q_partition(std::vector<T> &v, std::size_t begin, std::size_t end);
-
-template <typename T>
-void quick_sort(std::vector<T> &v) {
-  q_sort(v, 0, v.size());
-}
-
-template <typename T>
-void q_sort(std::vector<T> &v, std::size_t begin, std::size_t end) {
-  if (begin >= end) return;
-  std::size_t idx = q_partition(v, begin, end);
-  q_sort(v, begin, idx);
-  q_sort(v, idx + 1, end);
-}
-
-template <typename T>
-std::size_t q_partition(std::vector<T> &v, std::size_t begin, std::size_t end) {
+template <typename T, class Compare>
+std::size_t _q_partition(std::vector<T>& v, std::size_t begin, std::size_t end,
+                         const Compare& cmp) {
   std::size_t low = begin, high = end - 1;
   auto pivot = v[high];
   do {
-    while (low < high && v[low] <= pivot) ++low;
+    while (low < high && cmp(v[low], pivot)) ++low;
     if (low < high) {
       v[high] = v[low];
       --high;
     }
-    while (low < high && v[high] >= pivot) --high;
+    while (low < high && cmp(pivot, v[high])) --high;
     if (low < high) {
       v[low] = v[high];
       ++low;
@@ -45,40 +28,50 @@ std::size_t q_partition(std::vector<T> &v, std::size_t begin, std::size_t end) {
   return low;
 }
 
+template <typename T, class Compare>
+void _q_sort(std::vector<T>& v, std::size_t begin, std::size_t end,
+             const Compare& cmp) {
+  if (begin >= end) return;
+  std::size_t idx = _q_partition(v, begin, end, cmp);
+  _q_sort(v, begin, idx, cmp);
+  _q_sort(v, idx + 1, end, cmp);
+}
+
+template <typename T, class Compare = std::less<>>
+void quick_sort(std::vector<T>& v, const Compare& cmp = std::less<>()) {
+  _q_sort(v, 0, v.size(), cmp);
+}
+
 }  // namespace
 
 namespace quick_sort_2 {
 
-template <typename T>
-void q_sort(std::vector<T> &v, std::size_t begin, std::size_t end);
-
-template <typename T>
-std::size_t q_partition(std::vector<T> &v, std::size_t begin, std::size_t end);
-
-template <typename T>
-void quick_sort(std::vector<T> &v) {
-  q_sort(v, 0, v.size());
-}
-
-template <typename T>
-void q_sort(std::vector<T> &v, std::size_t begin, std::size_t end) {
-  if (begin >= end) return;
-  std::size_t idx = q_partition(v, begin, end);
-  q_sort(v, begin, idx);
-  q_sort(v, idx + 1, end);
-}
-
-template <typename T>
-std::size_t q_partition(std::vector<T> &v, std::size_t begin, std::size_t end) {
+template <typename T, class Compare>
+std::size_t _q_partition(std::vector<T>& v, std::size_t begin, std::size_t end,
+                         const Compare& cmp) {
   std::size_t low = begin;
   for (auto high = begin; high < end - 1; ++high) {
-    if (v[high] <= v[end - 1]) {
+    if (cmp(v[high], v[end - 1])) {
       std::swap(v[low], v[high]);
       ++low;
     }
   }
   std::swap(v[low], v[end - 1]);
   return low;
+}
+
+template <typename T, class Compare>
+void _q_sort(std::vector<T>& v, std::size_t begin, std::size_t end,
+             const Compare& cmp) {
+  if (begin >= end) return;
+  std::size_t idx = _q_partition(v, begin, end, cmp);
+  _q_sort(v, begin, idx, cmp);
+  _q_sort(v, idx + 1, end, cmp);
+}
+
+template <typename T, class Compare = std::less<>>
+void quick_sort(std::vector<T>& v, const Compare& cmp = std::less<>()) {
+  _q_sort(v, 0, v.size(), cmp);
 }
 
 }  // namespace quick_sort_2
--- sort/quick_sort_test.cpp
+++ sort/quick_sort_test.cpp
@@ -1,5 +1,6 @@
 #include "quick_sort.h"
 
+#include <functional>
 #include <vector>
 
 #include "vector.h"
@@ -12,6 +13,16 @@ int main() {
     quick_sort(v1);
     quick_sort_2::quick_sort(v2);
     std::sort(v_gt.begin(), v_gt.end());
+    assert_vector_eq(v1, v_gt);
+    assert_vector_eq(v2, v_gt);
+  }
+  for (int i = 0; i < 10; ++i) {
+    std::vector<int> v1 = random_int_vector();
+    std::vector<int> v2 = v1;
+    std::vector<int> v_gt = v1;
+    quick_sort(v1, std::greater<>());
+    quick_sort_2::quick_sort(v2, std::greater<>());
+    std::sort(v_gt.begin(), v_gt.end(), std::greater<>());
     assert_vector_eq(v1, v_gt);
     assert_vector_eq(v2, v_gt);
   }
